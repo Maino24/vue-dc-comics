e9b04b6b466a98f6a7e951310e3eaabd7d6ef4a8
Maino24 <mg02.gabriele@gmail.com>
2022-11-23 16:24:59 +0100

    header

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,8 +1,26 @@
 <template>
   <div>
-    <dir>
-      <img src="../assets/img/dc-logo.png" alt="">
-    </dir>
+    <div class="containerHeader">
+      <div>
+        <img src="../assets/img/dc-logo.png" alt="">
+      </div>
+      <div>
+        <nav>
+          <ul>
+            <li>CHARACTERS</li>
+            <li>COMICS</li>
+            <li>MOVIE</li>
+            <li>TV</li>
+            <li>GAMES</li>
+            <li>COLLECTIBLES</li>
+            <li>VIDEOS</li>
+            <li>FANS</li>
+            <li>NEWS</li>
+            <li>SHOP</li>
+          </ul>
+        </nav>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,18 +33,28 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+
+.containerHeader{
+  width: 70%;
+  margin: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  justify-content: space-between;
 }
+
+img{
+  width: 80%;
+}
+
 ul {
   list-style-type: none;
   padding: 0;
 }
 li {
   display: inline-block;
-  margin: 0 10px;
+  margin: 0 5px;
+  font-size: 0.8rem;
 }
-a {
-  color: #42b983;
-}
+
 </style>
