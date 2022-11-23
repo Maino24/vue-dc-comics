ebb6d1ccc4e2a479019257a39f6a33b967a77f66
Maino24 <mg02.gabriele@gmail.com>
2022-11-23 16:09:09 +0100

    headerComp

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <HeaderComp/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComp from './components/HeaderComp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderComp
   }
 }
 </script>
