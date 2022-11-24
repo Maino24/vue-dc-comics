4bcf07095e3cbad80ab6693b9e7526d5bbfab396
Maino24 <mg02.gabriele@gmail.com>
2022-11-24 20:49:52 +0100

    border appvue

--- src/App.vue
+++ src/App.vue
@@ -23,9 +23,19 @@ export default {
 </script>
 
 <style lang="scss">
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
 
-
+ font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
 
 }
 </style>
